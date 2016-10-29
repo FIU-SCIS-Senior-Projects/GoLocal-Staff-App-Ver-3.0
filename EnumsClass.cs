f76cc1cd276889518f0b3d559b74225ed1019233
dgonz324 <dgonz324@fiu.edu>
2016-10-28 22:43:49 -0400

    Add files via upload

---
+++ EnumsClass.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoLocal
+{
+    public enum States
+    {
+        State,
+        AL,
+        AK,
+        AZ,
+        AR,
+        CA,
+        CO,
+        CT,
+        DE,
+        FL,
+        GA,
+        HI,
+        ID,
+        IL,
+        IN,
+        IA,
+        KS,
+        LA,
+        ME,
+        MD,
+        MI,
+        MN,
+        MS,
+        MO,
+        MT,
+        NE,
+        NV,
+        NH,
+        NJ,
+        NM,
+        NY,
+        NC,
+        ND,
+        OH,
+        OK,
+        OR,
+        PA,
+        RI,
+        SC,
+        SD,
+        TN,
+        TX,
+        UT,
+        VT,
+        VA,
+        WA,
+        WV,
+        WI,
+        WY
+    }
+    public enum Genders
+    {
+        Gender,
+        Female,
+        Male
+    }
+    public enum StaffTypes
+    {
+        Type,
+        Model,
+        BrandAmbassador,
+        FlyerDistributor,
+        FieldMarketingManager,
+        SeniorMarketingManager,
+        Dancer,
+        Bartender,
+        Waitress,
+        ProductionAssistant,
+        SalesExecutive,
+        Photographer,
+        DJ,
+        Electrician,
+        GraphicDesigner,
+        SoundEngineer,
+        Band,
+        Other
+    }
+    
+
+}
